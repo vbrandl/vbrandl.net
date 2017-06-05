aa3acc264c102c4dc273252a2a41024afb04fe25
Valentin Brandl <vbrandl@riseup.net>
2017-06-05 11:40:22 +0200

    Add prepare script

---
+++ prepare.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+HUGO_BIN="hugo"
+
+# build static pages
+${HUGO_BIN}
+
+# ahead of time compression and timestamp fixing
+find public/ -type f \( -name '*.html' -o -name '*.js' -o -name '*.css' -o -name '*.xml' -o -name '*.svg' \) -exec gzip -v -k -f --best {} \; -exec touch -r {} "{}.gz" \;
